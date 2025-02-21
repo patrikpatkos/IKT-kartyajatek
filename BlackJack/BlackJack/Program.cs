df5ec70eaddee420e36b3c3b0c0f1fb39966b10f
patrikpatkos <145753272+patrikpatkos@users.noreply.github.com>
2025-02-21 15:31:46 +0100

    Add files via upload

---
+++ BlackJack/BlackJack/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Blackjack
+{
+    static Random random = new Random();
+    static string[] suits = { "♥", "♦", "♠", "♣" };
+    static string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
+    static void Main()
+    {
+        Console.WriteLine("\r\n███████████████████████████████████████████████████████\r\n█▄─▄─▀█▄─▄████▀▄─██─▄▄▄─█▄─█─▄███▄─▄██▀▄─██─▄▄▄─█▄─█─▄█\r\n██─▄─▀██─██▀██─▀─██─███▀██─▄▀██─▄█─███─▀─██─███▀██─▄▀██\r\n▀▄▄▄▄▀▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀\n");
+        int penz = 5000;
+        Console.WriteLine("\nRules:\n~ Try to get as close to 21 without going over. \n~ Dealer stops hitting at 17.\n");
+
+        while (penz > 0)
+        {
+            Console.WriteLine($"Money: {penz}");
+            int tet = GetBet(penz);
+
+
+            static int GetBet(int maxTet)
+            {
+                int tet;
+                do
+                {
+                    Console.Write("Enter your bet (1 - 5000): ");
+                } while (!int.TryParse(Console.ReadLine(), out tet) || tet < 1 || tet > maxTet);
+                return tet;
+            }
+        }
+    }
+}
